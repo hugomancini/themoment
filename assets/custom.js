5a9ac28eddcce28680a7cc7e7f1782cbae44c781
Hugo MANCINI <h.g.mancini@gmail.com>
2019-12-11 12:59:41 +0100

    Hey

---
+++ assets/custom.js
@@ -0,0 +1,56 @@
+/**
+ * Include your custom JavaScript here.
+ *
+ * We also offer some hooks so you can plug your own logic. For instance, if you want to be notified when the variant
+ * changes on product page, you can attach a listener to the document:
+ *
+ * document.addEventListener('variant:changed', function(event) {
+ *   var variant = event.detail.variant; // Gives you access to the whole variant details
+ * });
+ *
+ * You can also add a listener whenever a product is added to the cart:
+ *
+ * document.addEventListener('product:added', function(event) {
+ *   var variant = event.detail.variant; // Get the variant that was added
+ *   var quantity = event.detail.quantity; // Get the quantity that was added
+ * });
+ */
+
+// var height = $('.Header').height();
+
+// function resizeSlideshow (){
+// 	$('.template-index #section-slideshow .flickity-viewport').css({ 'height': 'calc(100vh - ' + height + 'px) !important' });
+// }
+
+// resizeSlideshow();
+
+// $(window).scroll(function(){
+// 	resizeSlideshow();
+// })
+
+// $(window).resize(function(){
+// 	resizeSlideshow();
+// })
+
+$(function(){
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
